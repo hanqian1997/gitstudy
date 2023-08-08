74a592c5e35a870ca3219d9b095c3a6678cdf34c
hanqian1997 <2210202992@qq.com>
2023-08-08 16:03:48 +0800

    add signal out

--- a_and_b.v
+++ a_and_b.v
@@ -2,9 +2,11 @@ module a_and_b (
     input       a,
     input       b,
     output      c,
-    output	d
+    output	d,
+    output	out
 );
 
 and (c,a,b);
 assign d = a^b;
+or(out,a,b);
 endmodule
