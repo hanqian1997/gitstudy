c852103e96e7ed3a6cf39439a3343b284ecfa2b3
hanqian1997 <2210202992@qq.com>
2023-08-08 16:06:39 +0800

    add out assign

--- a_and_b.v
+++ a_and_b.v
@@ -2,9 +2,11 @@ module a_and_b (
     input       a,
     input       b,
     output      c,
-    output	d
+    output	d,
+    output	out
 );
 
 and (c,a,b);
 assign d = a^b;
+assign ouit = a | b;
 endmodule
