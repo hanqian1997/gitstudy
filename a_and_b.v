50a0fd38b9ad532d9eeaae4f164cfb219244749f
hanqian1997 <2210202992@qq.com>
2023-08-08 17:05:34 +0800

    add out_not and it is not gate

--- a_and_b.v
+++ a_and_b.v
@@ -4,11 +4,13 @@ module a_and_b (
     output      c,
     output	    d,
     output	    out,
-    output      y
+    output      y,
+    output      out_not
 );
 
 and (c,a,b);
 assign d = a^b;
 assign out = a | b;
 or (y,a,b);
+not(a,out_not);
 endmodule
