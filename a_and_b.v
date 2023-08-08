91018c249e5bb3af2cc6e08fe90ae15afc3c9fb6
hanqian1997 <2210202992@qq.com>
2023-08-08 16:46:19 +0800

    add y is or gate

--- a_and_b.v
+++ a_and_b.v
@@ -2,11 +2,13 @@ module a_and_b (
     input       a,
     input       b,
     output      c,
-    output	d,
-    output	out
+    output	    d,
+    output	    out,
+    output      y
 );
 
 and (c,a,b);
 assign d = a^b;
 assign out = a | b;
+or (y,a,b);
 endmodule
